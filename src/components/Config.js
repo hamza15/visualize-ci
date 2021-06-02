77f2495dca429a1aa2b6adc0708e68b0bd6c5fa2
Hamza Naqi <sardan5@vt.edu>
2021-06-02 09:36:07 -0400

    first commit

---
+++ src/components/Config.js
@@ -0,0 +1,17 @@
+import { React } from 'react'
+
+const Config = (
+    {
+        elements
+    }) => {
+
+        return (
+            <div>
+                <h1>Hi</h1>
+            </div>
+        )
+            
+
+}
+
+export default Config;
