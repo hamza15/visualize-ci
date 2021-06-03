8c15d1bfb480ca83a75180b48604ce71d03af9ad
Hamza Naqi <sardan5@vt.edu>
2021-06-03 17:45:52 -0400

    drag'n'drop

--- src/components/DrawPipeline.js
+++ src/components/DrawPipeline.js
@@ -1,5 +1,8 @@
-import React, { useState, Fragment } from 'react';
-import ReactFlow, {addEdge, Background, Controls} from 'react-flow-renderer';
+import React, { useState, useRef } from 'react';
+import ReactFlow, {addEdge, ReactFlowProvider, removeElements, Background, Controls} from 'react-flow-renderer';
+import Sidebar from './Sidebar';
+import './css/dnd.css';
+
 
 const initialElements = [
     {
@@ -15,54 +18,88 @@ const initialElements = [
     }
 ]
 
-const onLoad = (reactFlowInstance) => {
-    reactFlowInstance.fitView();
-}
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
 
 const DrawPipeline = () => {
 
+    const reactFlowWrapper = useRef(null);
+    const [reactFlowInstance, setReactFlowInstance] = useState(null);
+
+    const onElementsRemove = (elementsToRemove) =>
+    setElements((els) => removeElements(elementsToRemove, els));
+
+    const onLoad = (_reactFlowInstance) =>
+    setReactFlowInstance(_reactFlowInstance);
+
+  const onDragOver = (event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+  };
+
+  const onDrop = (event) => {
+    event.preventDefault();
+
+    const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+    const type = event.dataTransfer.getData('application/reactflow');
+    const position = reactFlowInstance.project({
+      x: event.clientX - reactFlowBounds.left,
+      y: event.clientY - reactFlowBounds.top,
+    });
+    const newNode = {
+      id: getId(),
+      type,
+      position,
+      data: { label: `${type} node` },
+    };
+
+    setElements((es) => es.concat(newNode));
+  };
+
     const [elements, setElements] = useState(initialElements)
-    const [name, setName] = useState("")
+    // const [name, setName] = useState("")
 
 
-    const addNode = () => {
-        setElements(e => e.concat({
-            id: (e.length+1).toString(),
-            data: {label: `${name}`},
-            position: {x: 100, y: 45} 
-        }))
-    }
+    // const addNode = () => {
+    //     setElements(e => e.concat({
+    //         id: (e.length+1).toString(),
+    //         data: {label: `${name}`},
+    //         position: {x: 100, y: 45} 
+    //     }))
+    // }
 
     const onConnect = (params) => setElements(e => addEdge(params,e));
 
     return(
-        <Fragment>
-                <ReactFlow
-                    elements={elements}
-                    onLoad={onLoad}
-                    onConnect={onConnect}
-                    connectionLineType= "bezier"
-                    // snapToGrid= {true}
-                    // snapGrid={[16,16]}
-                    style={{width: '100%', height: '90vh'}}
-                >
-                    <Controls />
-                    <Background 
-                        color='#888'
-                    />
-            </ReactFlow>
+        // <Fragment>
+        <div className="dndflow">
+            <ReactFlowProvider>
+                <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+                    <ReactFlow
+                        elements={elements}
+                        onLoad={onLoad}
+                        onConnect={onConnect}
+                        connectionLineType= "bezier"
+                        // snapToGrid= {true}
+                        // snapGrid={[16,16]}
+                        style={{width: '100%', height: '90vh'}}
 
-            <div>
-                <input type="text" placeholder="Test Stage"
-                onChange={e => setName(e.target.value)}
-                name="title"/>
-                <button type="button"
-                onClick={addNode}
-                >
-                Add Stage
-                </button>
-            </div>
-            </Fragment>
+                        onElementsRemove={onElementsRemove}
+                        onDrop={onDrop}
+                        onDragOver={onDragOver}
+                    >
+                        <Controls />
+                        <Background 
+                            color='#888'
+                        />
+                    </ReactFlow>
+                    </div>
+                <Sidebar />
+                </ReactFlowProvider>
+        </div>
+            // </Fragment>
     )
 }
 
